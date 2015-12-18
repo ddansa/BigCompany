7ca553c967496a94934bea4460262371863c1a71
ddansa <anders@hvid.se>
2015-12-18 12:45:40 +0100

    Initial Magic

---
+++ BigCompanyLab/src/BigCompanyLab/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using BigCompanyLab.Models;
+using BigCompanyLab.Models.Classes;
+
+
+namespace BigCompanyLab.Controllers
+{
+    public abstract class MainController : Controller
+    {
+        protected static DataManager _data = new DataManager();
+    }
+}
---
+++ BigCompanyLab/src/BigCompanyLab/Models/Classes/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigCompanyLab.Models.Classes
+{
+    public class Company
+    {
+        public Company(string companyName, string address, int employeecount)
+        {
+            CompanyName = companyName;
+            Address = address;
+            EmployeeCount = employeecount;
+        }
+
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public int EmployeeCount { get; set; }
+
+    }
+}
